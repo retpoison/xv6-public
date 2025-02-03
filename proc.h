cca615a877d89241b42043f0df8c7e9cdf665c49
retpoison <standardret@proton.me>
2025-02-03 22:23:28 +0330

    added cs and wait to proc struct

--- proc.h
+++ proc.h
@@ -48,6 +48,8 @@ struct proc {
 	int killed;		// If non-zero, have been killed
 	struct file *ofile[NOFILE];	// Open files
 	struct inode *cwd;	// Current directory
+	uint cs;
+	uint wait;
 	char name[16];		// Process name (debugging)
 };
 
